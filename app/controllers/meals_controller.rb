94d0e29e62fe3f34075c4f225f739d2ee4235965
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-03-28 08:05:59 -0400

    add delete to meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -16,11 +16,33 @@ class MealsController < ApplicationController
   def create
     @meal = Meal.new(meal_params)
     if @meal.save
-      redirect_to @meal
-    else 
+      redirect_to meals_path
+    else
       render :new
     end
   end
+
+  def edit 
+    @meal = Meal.find(params[:id])
+  end
+
+  def update
+    @meal = Meal.find(params[:id])
+      @meal.update(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else 
+      redirect_to meals_path
+    end
+  end
+  
+  def destroy
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+    redirect_to meals_path
+  end
+  
+
 private 
   def meal_params
     params.require(:meal).permit(:title)
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :user
+  has_many :meals
+  
 end
