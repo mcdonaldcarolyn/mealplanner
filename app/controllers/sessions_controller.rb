47932aae0db1aa456e5fa046454b45a852234995
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-04-10 13:31:22 -0400

    reset to before extra omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,18 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(email: auth['info']['email']) do |u|
-      # u.name = auth['info']['name']
+    @user = User.find_by(:email => auth['info']['email'])
+
+    if !@user || !@user.id
+      @user = User.create({
+        :email => auth['info']['email'],
+        :password => "123123123",
+        :password_confirmation => "123123123",
+        :created_at => Time.new,
+        :updated_at => Time.new
+      })
     end
- 
-    session[:user_id] = @user.id
- 
-    render 'meals/index'
+
+    sign_in_and_redirect(:user, @user)
   end
  
   private
--- app/models/meals_ingredient.rb
+++ app/models/meals_ingredient.rb
@@ -2,7 +2,7 @@ class MealsIngredient < ApplicationRecord
   belongs_to :meal
   belongs_to :ingredient
   accepts_nested_attributes_for :ingredient
-  validates :name, presence: true, uniqueness: true
+  
 
   def ingredients=(ingredient_attribute)
     ingredient = Ingredient.find_or_create_by(ingredient_attribute)
